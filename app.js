ea1e857898e2ea2dd0a69569a6d28636351fd034
guweimo <lyfw244885@hotmail.com>
2018-07-05 16:49:34 +0800

    去掉输出日志。

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.all('*', (req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Credentials", true); //可以带cookies
   res.header('X-Powered-By', '3.2.1');
-  console.log(req.method);
+  
   if (req.method == 'OPTINOS') {
     res.send(200);
   } else {
