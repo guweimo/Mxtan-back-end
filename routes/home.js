51fa9b5c3bd3a9e0271e4c0ee0910a24dad4bbdd
guweimo <lyfw244885@hotmail.com>
2018-07-06 16:56:29 +0800

    设置成功状态和错误状态

--- routes/home.js
+++ routes/home.js
@@ -1,32 +1,44 @@
 import moment from 'moment'
 import express from 'express'
 import db from '../config/db'
-const router = express.Router();
 
-// const db = require('../config/db');
+const router = express.Router()
 
 router.get('/navlist', function (req, res, next) {
-    db.query('select * from nav', function (err, result) {
+    db.query('select * from nav where state=1', function (err, result) {
         if (err) {
-            res.send(err);
+            res.send({
+                status: 2001,
+                message: err.message
+            })
         } else {
-            res.send(result);
+            res.send({
+                status: 2000,
+                data: result
+            })
         }
     });
 });
 
 router.post('/list', (req, res, next) => {
-    db.query('select * from article where state=1 order by `current_time` desc ', (err, result) => {
+    let sql = 'select * from article where state=1 order by `current_time` desc'
+    db.query(sql, (err, result) => {
         if (err) {
-            res.send(err);
+            res.send({
+                status: 2000,
+                message: err.message
+            })
         } else {
-            let data = result;
+            let data = result
             for (let arr of data) {
                 arr.date = moment(arr.current_time).format('YYYY-MM-DD')
                 arr.current_time = moment(arr.current_time).format('YYYY-MM-DD HH:mm:ss')
                 arr.update_time = moment(arr.update_time).format('YYYY-MM-DD HH:mm:ss')
             }
-            res.send(data);
+            res.send({
+                status: 2000,
+                data
+            })
         }
     })
 })
