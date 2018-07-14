1c0c51fb19b96d24bfe6ced0d567adde2b316b3a
guweimo <lyfw244885@hotmail.com>
2018-07-14 17:23:41 +0800

    update

--- app.js
+++ app.js
@@ -19,17 +19,20 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.all('*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With')
-  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
-  res.header("Access-Control-Allow-Credentials", true) //可以带cookies
-  res.header('X-Powered-By', '3.2.1')
-  
-  if (req.method == 'OPTINOS') {
-    res.send(200)
-  } else {
-    next()
-  }
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Content-Length, Authorization, Accept, X-Requested-With'
+    )
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Credentials', true) //可以带cookies
+    res.header('X-Powered-By', '3.2.1')
+
+    if (req.method == 'OPTINOS') {
+        res.send(200)
+    } else {
+        next()
+    }
 })
 
 // app.use('/api/', indexRouter);
@@ -62,17 +65,17 @@ var server = http.createServer(app)
 server.listen(port)
 
 function normalizePort(val) {
-  var port = parseInt(val, 10)
+    var port = parseInt(val, 10)
 
-  if (isNaN(port)) {
-    // named pipe
-    return val
-  }
+    if (isNaN(port)) {
+        // named pipe
+        return val
+    }
 
-  if (port >= 0) {
-    // port number
-    return port
-  }
+    if (port >= 0) {
+        // port number
+        return port
+    }
 
-  return false
+    return false
 }
