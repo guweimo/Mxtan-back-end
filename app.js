fb6792db68766b628146f353c6393a0c34325ce3
guweimo <lyfw244885@hotmail.com>
2019-03-07 16:13:19 +0800

    update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 import router from './routes/index.js'
 import session from 'express-session'
+import history from 'connect-history-api-fallback'
 
 var app = express()
 
@@ -18,6 +19,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(history())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.all('*', (req, res, next) => {
